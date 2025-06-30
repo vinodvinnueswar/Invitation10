6e7e2c978dfb30001f87e916dffe9e928a70b5e8
Sasispecious <sasispecious@gmail.com>
2025-06-30 13:21:21 -0700

    Adding Social Share

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -3,15 +3,23 @@ import React from 'react'
 import facebook from '../Images/Social_network/Facebook.png'
 import Instagram from '../Images/Social_network/Instagram.png'
 import whatsapp from '../Images/Social_network/Whatsapp1.png'
+import Share from './Share'
 
 const Contact = () => {
+    
+    
+
   return (
          <div className="social_Share">
                 <div className="wish">
                     <h3>Thank You for Visting</h3>
                 </div>
                 <h3>Social Share</h3>
-           <div className="network">
+
+                <div className="center">
+                <Share/>
+                </div>
+           {/* <div className="network">
            <div className="facebook">
                 <img src={facebook} alt="" />
             </div>
@@ -21,7 +29,7 @@ const Contact = () => {
             <div className="whatsapp">
                 <img src={whatsapp} alt="" />
             </div>
-           </div>
+           </div> */}
             <div className="details_1">
                 <img src="" alt="" />
                 <p className='p1'>InviteDigitly Online</p>
